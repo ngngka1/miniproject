74f2410afd967063c37ab412d9d71d92ee6bdba2
ngngka1 <ngwewe2015@gmail.com>
2023-11-18 22:02:04 +0800

    finished

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from PIL import ImageColor
 import os
-from functools import reduce
-import json
-import re
 import sys
+import json
 import time
 import random
+#unused modules vvv
 from copy import copy
+from functools import reduce
+import re 
 
 class sprite_type: 
     def __init__(self, name: str, width: int, height: int, default_x: int, default_y: int, file_basename: str):
@@ -55,8 +56,8 @@ class sprite_type:
                 self.sprint = False
     
     def movement_prompt(self):
-        prev_position_x = copy(self.position_x)
-        prev_position_y = copy(self.position_y)
+        prev_position_x = self.position_x
+        prev_position_y = self.position_y
         if self.move_right:
             self.position_x += 3
         if self.move_left:
@@ -101,6 +102,9 @@ class wall_object_type:
         self.hitbox = pygame.Rect(self.position_x, self.position_y, self.width, self.height)
            
 class obstacle_type(wall_object_type):
+    def __init__(self, param1, param2, param3, param4, param5, param6, param7, appearance_checkpoint):
+        wall_object_type.__init__(self, param1, param2, param3, param4, param5, param6, param7)
+        self.appearance_checkpoint = appearance_checkpoint
     def move(self, move_by_x: int, move_by_y: int):
         self.position_x += move_by_x
         self.position_y += move_by_y
@@ -127,7 +131,7 @@ class operation_option_type:
         self.operator = self.available_arithmetic_operators[random.randint(0, len(self.available_arithmetic_operators) - 1)]
         self.number = random.randint(1, maximum_operation_number) 
         self.operation = self.operator + str(self.number)
-        self.txt_surface = FONT.render(self.operation, True, ImageColor.getrgb("black"), ImageColor.getrgb("green"))
+        self.txt_surface = FONT_LARGE.render(self.operation, True, ImageColor.getrgb("black"), ImageColor.getrgb("green"))
         self.txt_surface_rect = self.txt_surface.get_rect(center = ((abs(margin_rect_width * int(self.position_x / (margin_rect_width / 2)) - margin_width) + (margin_rect_width / 2)) / 2, self.position_y))
         self.hitbox = pygame.rect.Rect(self.txt_surface_rect)
     
@@ -168,7 +172,7 @@ class gamestats:
         self.maximum_sum = 1
         
     def maximum_change(self, num):
-        maximum_num = ( max(calculation(num, left_operation.operation), calculation(num, right_operation.operation)) + min(calculation(num, left_operation.operation), calculation(num, right_operation.operation)) ) / 2
+        maximum_num = max(calculation(num, left_operation.operation), calculation(num, right_operation.operation))
         if maximum_num > num:
             return maximum_num
         else:
@@ -186,14 +190,14 @@ def load_sprite_property():
 
 def load_font_property():
     with open("font.json") as font_file:
-        global font_size
-        global font_type_path
-        global FONT
+        global FONT_LARGE, FONT_SMALL
         font_prop_dict = json.loads(font_file.read())# font property dictionary
-        font_size = font_prop_dict["Font_size"]
+        font_size_large = font_prop_dict["font_size_large"]
+        font_size_small = font_prop_dict["font_size_small"]
         font_type_path = os.path.join("assets", font_prop_dict["ttf_file_basename"])
         pygame.font.init()
-        FONT = pygame.font.Font(font_type_path, font_size)
+        FONT_LARGE = pygame.font.Font(font_type_path, font_size_large)
+        FONT_SMALL = pygame.font.Font(font_type_path, font_size_small)
 
 def load_wall_objects():
     global wall_objects
@@ -205,9 +209,9 @@ def load_wall_objects():
                 
                 for obstacle_object in level_prop["obstacles"]:
                     if obstacle_object["color"]:
-                        wall_objects.append(obstacle_type(obstacle_object["name"], obstacle_object["position_x"], obstacle_object["position_y"], obstacle_object["width"], obstacle_object["height"], obstacle_object["color"], None))
+                        wall_objects.append(obstacle_type(obstacle_object["name"], obstacle_object["position_x"], obstacle_object["position_y"], obstacle_object["width"], obstacle_object["height"], obstacle_object["color"], None, obstacle_object["appearance_checkpoint"]))
                     elif obstacle_object["image_file_basename"]:
-                        wall_objects.append(obstacle_type(obstacle_object["name"], obstacle_object["position_x"], obstacle_object["position_y"], obstacle_object["width"], obstacle_object["height"], None, obstacle_object["image_file_basename"]))
+                        wall_objects.append(obstacle_type(obstacle_object["name"], obstacle_object["position_x"], obstacle_object["position_y"], obstacle_object["width"], obstacle_object["height"], None, obstacle_object["image_file_basename"], obstacle_object["appearance_checkpoint"]))
                 
                 for margin_object in level_prop["margin"]:
                     
@@ -226,8 +230,6 @@ def load_wall_objects():
                         global margin_rect_height
                         margin_rect_height = screen_height
                         
-                
-    
 def load_level_attribute():
     global maximum_operation_number
     global speed
@@ -241,8 +243,8 @@ def load_level_attribute():
                 maximum_operation_number = level_prop["maximum_operation_number"]
                 speed = level_prop["speed"]
                 arithmetic_operators = level_prop["arithmetic_operators"]
-                left_operation = operation_option_type(200, 0, arithmetic_operators)
-                right_operation = operation_option_type(screen_width / 2 + 100, 0, arithmetic_operators)
+                left_operation = operation_option_type(margin_rect_width / 2 - 100, 0, arithmetic_operators)
+                right_operation = operation_option_type(margin_rect_width / 2 + 100, 0, arithmetic_operators)
                 left_operation.generate_random_operation()
                 right_operation.generate_random_operation()
                 boss = boss_type(level_prop["boss"]["name"], level_prop["boss"]["position_x"], level_prop["boss"]["position_y"], level_prop["boss"]["width"], level_prop["boss"]["height"], level_prop["boss"]["image_file_basename"], level_prop["boss"]["difficulty_scale"])
@@ -255,10 +257,8 @@ def load_stats():
 def operation_check():
     if sprite.hitbox.colliderect(left_operation.hitbox):
         stats.sum_counter = calculation(stats.sum_counter, left_operation.operation)
-        stats.checkpoints_counter += 1
     elif sprite.hitbox.colliderect(right_operation.hitbox):
         stats.sum_counter = calculation(stats.sum_counter, right_operation.operation)
-        stats.checkpoints_counter += 1
     else:
         return False
     return True
@@ -277,7 +277,7 @@ def draw_main_screen():
     screen.fill(screen_bgcolor)
     
     # print the operation / boss
-    if stats.checkpoints_counter <= 10:
+    if stats.checkpoints_counter < 10:
         pygame.draw.rect(screen, ImageColor.getrgb("green"), left_operation.hitbox, 0) # left
         screen.blit(left_operation.txt_surface, left_operation.txt_surface_rect)
         
@@ -285,6 +285,9 @@ def draw_main_screen():
         screen.blit(right_operation.txt_surface, right_operation.txt_surface_rect)
     else:
         screen.blit(boss.surface_scaled, boss.surface_scaled_rect)
+        boss_health_surface = FONT_LARGE.render(str(int(boss.health)), True, ImageColor.getrgb("black"))
+        boss_health_surface_rect = boss_health_surface.get_rect(center = ((margin_rect_width) / 2, boss.position_y))
+        screen.blit(boss_health_surface, boss_health_surface_rect)
     
     # print the wall objects
     for wall in wall_objects:
@@ -297,16 +300,32 @@ def draw_main_screen():
     screen.blit(sprite.surface_scaled, (sprite.position_x, sprite.position_y))
     
     # print stats on the right side of the screen
-    sum_counter_surface = FONT.render(str(stats.sum_counter), True, ImageColor.getrgb("black"))
-    maximum_sum_surface = FONT.render(str(stats.maximum_sum), True, ImageColor.getrgb("black"))
-    temp_boss_health_surface = FONT.render(str(boss.health), True, ImageColor.getrgb("black"))
-    screen.blit(sum_counter_surface, (margin_rect_width + 20, screen_height / 2))
-    screen.blit(maximum_sum_surface, (margin_rect_width + 20, screen_height / 2 + 50))
-    screen.blit(temp_boss_health_surface, (margin_rect_width + 20, screen_height / 2 + 100))
+    sum_counter_txt_surface = FONT_SMALL.render("Your current score:", True, ImageColor.getrgb("black"))
+    sum_counter_surface = FONT_LARGE.render(str(int(stats.sum_counter)), True, ImageColor.getrgb("black"))
+    screen.blit(sum_counter_txt_surface, (margin_rect_width + 20, screen_height / 2))
+    screen.blit(sum_counter_surface, (margin_rect_width + 20, screen_height / 2 + 50))
 
     pygame.display.update()
         
+def draw_level_transition_screen():
+    if level_passed:
+        txt_surface1 = FONT_SMALL.render(f"Congrats you have passed level {current_level}!", True, ImageColor.getrgb("black"))
+        txt_surface2 = FONT_SMALL.render("Press enter to continue to the next level", True, ImageColor.getrgb("black"))
+        txt_surface1_rect = txt_surface1.get_rect(center = (margin_rect_width / 2, margin_rect_height / 2))
+        txt_surface2_rect = txt_surface2.get_rect(center = (margin_rect_width / 2, margin_rect_height / 2 + 50))
+        screen.blit(txt_surface1, txt_surface1_rect)
+        screen.blit(txt_surface2, txt_surface2_rect)
+    else:
+        txt_surface1 = FONT_SMALL.render(f"You have failed level {current_level}!", True, ImageColor.getrgb("black"))
+        txt_surface2 = FONT_SMALL.render("Press enter to retry", True, ImageColor.getrgb("black"))
+        txt_surface1_rect = txt_surface1.get_rect(center = (margin_rect_width / 2, margin_rect_height / 2))
+        txt_surface2_rect = txt_surface2.get_rect(center = (margin_rect_width / 2, margin_rect_height / 2 + 50))
+        screen.blit(txt_surface1, txt_surface1_rect)
+        screen.blit(txt_surface2, txt_surface2_rect)
+    pygame.display.update()
+
 def main(): # for your information, in short, this function will be executed once the program is run
+    global current_level, level_passed, level_continue, level_failed
     running = True
     
     while running:
@@ -316,34 +335,52 @@ def main(): # for your information, in short, this function will be executed onc
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                level_continue = True
             sprite.movement_check(event)
-        
-        if stats.checkpoints_counter <= 10:
-            if operation_check() or left_operation.position_y > screen_height:
-                stats.maximum_sum = stats.maximum_change(stats.maximum_sum) # keep track of the largest sum possible
-                boss.health = (boss.health + stats.maximum_sum) / 2 * boss.difficulty_scale
-                left_operation.initialize()
-                right_operation.initialize()
-                left_operation.generate_random_operation()
-                right_operation.generate_random_operation()
-            else:
-                left_operation.move(0, speed)
-                right_operation.move(0, speed)
-                for wall in wall_objects:
-                    if type(wall) == obstacle_type:
-                        wall.move(0, speed)
-        else:
-            boss.move(0, speed / 3)
-            
-            
-        
         sprite.movement_prompt()
         
-        draw_main_screen()
+        if level_passed == False and level_failed == False:
+            if stats.checkpoints_counter < 10:
+                if operation_check() or left_operation.position_y > screen_height: # if operation hits player / reach margin
+                    stats.maximum_sum = stats.maximum_change(stats.maximum_sum) # keep track of the largest sum possible
+                    boss.health = (boss.health + stats.maximum_sum) / 2 * boss.difficulty_scale # divide (health + max result after operation) by 2 to avoid overly extreme value
+                    left_operation.initialize()
+                    right_operation.initialize()
+                    left_operation.generate_random_operation()
+                    right_operation.generate_random_operation()
+                    stats.checkpoints_counter += 1
+                else:
+                    left_operation.move(0, speed)
+                    right_operation.move(0, speed)
+                    for wall in wall_objects:
+                        if type(wall) == obstacle_type and (stats.checkpoints_counter == wall.appearance_checkpoint or wall.position_y > 0):
+                            wall.move(0, speed)
+            else:
+                boss.move(0, int(speed / 3))
+                if sprite.hitbox.colliderect(boss.hitbox):
+                    if stats.sum_counter >= boss.health:
+                        level_passed = True
+                    else:
+                        level_failed = True
+            draw_main_screen()
+        else:
+            if level_continue:
+                if not level_failed:
+                    current_level += 1
+                    load_wall_objects()
+                    load_level_attribute()
+                level_passed = False
+                level_continue = False
+                level_failed = False
+                load_stats()
+            else:
+                draw_level_transition_screen()
     
 
 ## The following lines are data of the screen window
-screen_width = 700
+screen_width = 1000
 screen_height = 700
 screen_bgcolor = ImageColor.getrgb("white") # PIL.ImageColor.getrgb(color) to get rgb value of color
 screen = pygame.display.set_mode((screen_width, screen_height)) # this line makes a screen with the width and height specified
@@ -358,6 +395,9 @@ load_font_property()
 
 ## other game property
 current_level = 1
+level_passed = False
+level_failed = False
+level_continue = False
 
 load_stats()
 load_wall_objects()
